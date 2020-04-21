da2bbca5c4ff6cdaecb59f6710abbd3ff560885a
iacopo ciao <iacopociao1992@gmail.com>
2020-04-21 23:00:50 +0200

    implemented empty method

--- generator/lib/src/dartstruct_generator.dart
+++ generator/lib/src/dartstruct_generator.dart
@@ -4,6 +4,7 @@ import 'package:code_builder/code_builder.dart';
 import 'package:dart_style/dart_style.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:dartstruct/dartstruct.dart';
+import './extensions/extensions.dart';
 
 class DartStructGenerator extends GeneratorForAnnotation<Mapper> {
   final _emitter = DartEmitter();
@@ -32,7 +33,7 @@ class DartStructGenerator extends GeneratorForAnnotation<Mapper> {
       ..name = '${classElement.displayName}Impl'
       ..abstract = false
       ..extend = refer(classElement.displayName)
-      ..methods.addAll(_generateMethods(classElement.methods));
+      ..methods.addAll(classElement.methods.where((method) => method.isAbstract).map(_generateMethod));
 
     final mapperImpl = classBuilder.build();
 
@@ -42,13 +43,78 @@ class DartStructGenerator extends GeneratorForAnnotation<Mapper> {
 
   }
 
-  List<Method> _generateMethods(List<MethodElement> methods) {
-    if (methods.any((method) => method.isAbstract)) {
-      throw InvalidGenerationSourceError('Not implemented yet',
-        todo: 'remove all methods'
+  Method _generateMethod(MethodElement method) {
+
+    if (method.parameters.isEmpty) {
+      throw InvalidGenerationSourceError('Method must provide an argument',
+        todo: 'add source parameter',
+        element: method
       );
     }
 
-    return <Method>[];
+    if (method.parameters.length != 1) {
+      throw InvalidGenerationSourceError('Method must provide only one argument',
+        todo: 'provide only one argument',
+        element: method
+      );
+    }
+
+    final source = method.parameters.first;
+
+    if (source.isNamed) {
+      throw InvalidGenerationSourceError('Named parameters are not supported',
+        todo: 'provide a positional argument',
+        element: source
+      );
+    }
+
+    final sourceType = source.type;
+    final returnType = method.returnType;
+
+    if (returnType.isPrimitive || sourceType.isPrimitive) {
+      throw InvalidGenerationSourceError('Primitive types are not supported',
+        element: method
+      );
+    }
+
+    if (sourceType.isFuture || returnType.isFuture) {
+      throw InvalidGenerationSourceError('Future types are not supported',
+        element: method
+      );
+    }
+
+    if (sourceType.isCollection || returnType.isCollection) {
+      throw InvalidGenerationSourceError('Collection types are not supported',
+        element: method
+      );
+    }
+
+    if (sourceType.isDynamic || returnType.isDynamic) {
+      throw InvalidGenerationSourceError('Dynamic type is not supported',
+        element: method
+      );
+    }
+
+    return Method((builder) {
+      builder
+        ..annotations.add(CodeExpression(Code('override')))
+        ..name = method.displayName
+        ..requiredParameters.add(_generateSourceParameter(source))
+        ..returns = refer(returnType.element.displayName)
+        ..body = _generateMethodBody(method);
+    });
   }
+
+  Parameter _generateSourceParameter(ParameterElement source) {
+    return Parameter((builder) {
+      builder
+        ..name = source.name
+        ..type = refer(source.type.element.displayName);
+    });
+  }
+
+  Code _generateMethodBody(MethodElement methodElement) {
+    return Code('return null;');
+  }
+
 }
