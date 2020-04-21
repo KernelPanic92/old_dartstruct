08b6ae22f2d2c3baa76b7aabce85ba6056e7b577
iacopo ciao <iacopociao1992@gmail.com>
2020-04-21 23:01:06 +0200

    created tests

---
+++ integration_tests/test/empty_method_mapper/empty_method_mapper.dart
@@ -0,0 +1,20 @@
+
+import 'package:dartstruct/dartstruct.dart';
+
+part 'empty_method_mapper.g.dart';
+
+class Model {
+  String field;
+}
+
+class Dto {
+  String field;
+}
+@Mapper()
+abstract class EmptyMethodMapper {
+
+  static EmptyMethodMapper get INSTANCE => EmptyMethodMapperImpl();
+
+  Dto modelToDto(Model model);
+
+}
---
+++ integration_tests/test/empty_method_mapper/empty_method_mapper_test.dart
@@ -0,0 +1,17 @@
+
+import 'package:test/test.dart';
+import 'empty_method_mapper.dart';
+
+void main() {
+  group('EmptyMethodMapper', () {
+
+    test('Should be instantiated', () {
+      expect(EmptyMethodMapper.INSTANCE, isNotNull);
+    });
+
+    test('Should return null', () {
+      expect(EmptyMethodMapper.INSTANCE, isNotNull);
+    });
+
+  });
+}
