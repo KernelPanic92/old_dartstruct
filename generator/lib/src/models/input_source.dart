d0bd9ebd32e35b35ab579b6610d65d94e7dff256
iacopo ciao <iacopociao1992@gmail.com>
2020-04-25 17:42:25 +0200

    created input and output source models

---
+++ generator/lib/src/models/input_source.dart
@@ -0,0 +1,10 @@
+import 'package:analyzer/dart/element/type.dart';
+
+class InputSource {
+
+  final String name;
+  final DartType type;
+
+  InputSource(this.type, this.name);
+
+}
---
+++ generator/lib/src/models/output_source.dart
@@ -0,0 +1,10 @@
+import 'package:analyzer/dart/element/type.dart';
+
+class OutputSource {
+
+  final String name;
+  final DartType type;
+
+  OutputSource(this.type, this.name);
+
+}
